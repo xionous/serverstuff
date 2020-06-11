1bc7a6324a48889c953a370a4733199b10297ef9
Matthew Streeter <Matthew.Streeter@sjrb.ca>
2020-06-11 08:33:02 -0500

    Changed CHG to alt key and removed functon from ctrl key

--- server.user.js
+++ server.user.js
@@ -3,7 +3,7 @@
 // @namespace all
 // @match *://*/*
 // @author Matthew Streeter
-// @version 0.0.9
+// @version 0.1.0
 // @downloadURL https://github.com/xionous/serverstuff/raw/master/server.user.js
 // @updateURL https://github.com/xionous/serverstuff/raw/master/server.user.js
 // @grant none
@@ -34,11 +34,8 @@
         var thetext = getSelectionText().trim()
         if (thetext.length > 0 && e.shiftKey) {
             copyText(thetext);
-        } else if (thetext.length > 0 && e.ctrlKey && !e.altKey) {
+        } else if (thetext.length > 0 && !e.altKey && e.ctrlKey) {
             var the_URL = "https://shawprod.service-now.com/nav_to.do?uri=%2Fchange_request_list.do%3Fsysparm_query%3D123TEXTQUERY321%253D" + thetext;
-            window.open(the_URL);
-        } else if (thetext.length > 0 && e.altKey && !e.ctrlKey) {
-            var the_URL = "https://shawprod.service-now.com/nav_to.do?uri=%2Fincident_list.do%3Fsysparm_query%3D123TEXTQUERY321%253D" + thetext;
             window.open(the_URL);
         } else if (thetext.length > 0 && e.altKey && e.ctrlKey) {
             var the_URL = "http://"+thetext;
